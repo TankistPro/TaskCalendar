ded9982f8cdbcd3cd654e5200fee6c6958e870e4
TankistPro <nakol.nayk@mail.ru>
2022-05-03 22:01:51 +0300

    TASK: Созданы основные слои для разделения монолитного приложения

---
+++ TaskCalendar.Domain.Interfaces/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskCalendare.Domain.Core;
+
+namespace TaskCalendar.Domain.Interfaces
+{
+    public interface ITaskRepository: IDisposable
+    {
+        IEnumerable<Task> GetTaskList();
+        bool CreateTask(Task entity);
+        bool DeleteTask(Task entity);
+    }
+}
---
+++ TaskCalendar.Infrastructure.Data/Context/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TaskCalendare.Domain.Core;
+
+namespace TaskCalendar.Infrastructure.Data.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Task> Tasks => Set<Task>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=taskCalendare.db");
+        }
+    }
+}
